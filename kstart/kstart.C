3a009a3df90921fd3865a70b0b6a6b3d84bcf3de
Matthias Ettrich <ettrich@troll.no>
1998-10-22 16:53:45 +0000

    yet another killer application for KDE ;-)
    kstart is a tiny utility to launch legay applications with special KDE
    window properties such as iconified, maximized, a certain virtual
    desktop, a special decoration or sticky.
    
    svn path=/trunk/kdebase/kstart/; revision=11631

---
+++ kstart/kstart.C
@@ -0,0 +1,182 @@
+/*
+ * kstart.C. Part of the KDE project.
+ *
+ * Copyright (C) 1997 Matthias Ettrich
+ *
+ */
+
+#include <qdir.h>
+
+#include <kstart.moc>
+#include "version.h"
+
+#include <fcntl.h>
+#include <kprocess.h>
+#include <qregexp.h>
+
+void execute(const char* cmd){
+  KShellProcess proc;
+  proc << cmd;
+  proc.start(KShellProcess::DontCare);
+}
+
+KStart::KStart(KWMModuleApplication* kwmmapp_arg,
+	       const char* command_arg,
+	       const char* window_arg,
+	       int desktop_arg,
+	       bool activate_arg,
+	       bool maximize_arg,
+	       bool iconify_arg,
+	       bool sticky_arg,
+	       int decoration_arg
+	       )
+  :QObject(){
+    kwmmapp = kwmmapp_arg;
+    command = command_arg;
+    window = window_arg;
+    desktop = desktop_arg;
+    activate = activate_arg;
+    maximize = maximize_arg;
+    iconify = iconify_arg;
+    sticky = sticky_arg;
+    decoration = decoration_arg;
+    
+    // just connect to the initialized() signal, we want to be
+    // informed if we recieved all existing windows
+    connect(kwmmapp, SIGNAL(initialized()), SLOT(initialized()));
+    kwmmapp->connectToKWM();
+}
+
+void KStart::initialized(){
+    // ok, we are initialized. Now connect to window add to get the NEW windows
+    connect(kwmmapp, SIGNAL(windowAdd(Window)), SLOT(windowAdd(Window)));
+    if (window) {
+	KWM::doNotManage(window);
+	XSync(qt_xdisplay(), False);
+    }
+    //finally execute the comand
+    execute(command);
+}
+
+void KStart::windowAdd(Window w){
+    if (window) {
+	QString t = KWM::title(w);
+	QRegExp r = window;
+	if (r.match(t) != -1){
+	    applyStyle( w );
+	    ::exit(0);
+	}
+    }
+    else {
+	// not window specified, just take the first one
+	applyStyle( w );
+	::exit(0);
+    }
+}
+
+void KStart::applyStyle(Window w) {
+    if (window)
+	KWM::prepareForSwallowing(w);
+    if (desktop >= 0) {
+	if (KWM::desktop(w) != desktop)
+	    KWM::moveToDesktop(w, desktop);
+    }
+    if (maximize) {
+	debug("do maximize");
+	KWM::doMaximize(w, true);
+    }
+    if (iconify)
+	KWM::setIconify(w, true);
+    if (sticky) {
+	KWM::setSticky(w, true);
+    }
+    if (decoration != KWM::normalDecoration) {
+	KWM::setDecoration(w, decoration);
+    }
+
+    XSync(qt_xdisplay(), False);
+    if (window) {
+	XMapWindow(qt_xdisplay(), w);
+	XSync(qt_xdisplay(), False);
+    }
+    if (activate)
+      KWM::activate(w);
+    XSync(qt_xdisplay(), False);
+}
+
+
+
+
+int main( int argc, char *argv[] )
+{
+  if (argc <= 2) {
+      printf(KSTART_VERSION);
+      printf("\n Copyright (C) 1997, 1998 Matthias Ettrich (ettrich@kde.org)\n");
+      printf("\n Utility to launch legay applications with special KDE window properties");
+      printf("\n such as iconified, maximized, a certain virtual desktop, a special decoration");
+      printf("\n or sticky");
+      printf("\n ");
+      printf("\n In addition, the -activate switch will jump to the window even if it is");
+      printf("\n started on a different virtual desktop");
+      printf("\n ");
+      printf("\n Usage:");
+      printf("\n %s <command> [-window <regular expression>] [-desktop <number>]", argv[0]);
+      printf("\n              [-sticky] [-iconify] [-maximize] ");
+      printf("\n              [-decoration tiny|none] [-activate]");
+      printf("\n ");
+      printf("\n If you do not specify a regular expression for the windows title," );
+      printf("\n then the very first window to appear will be taken. Not recommended!");
+      printf("\n ");
+      printf("\n Example usage:");
+      printf("\n %s \"xclock -geometry 80x80-0+0\" -window xclock -decoration tiny -sticky", argv[0]);
+      printf("\n puts a tiny decorated, sticky xclock on the top right corner of the screen.");
+      printf("\n ");
+      printf("\n ");
+
+      ::exit(0);
+  }
+  int desktop = 0;
+  char* window = 0;
+  bool activate = FALSE;
+  bool maximize = FALSE;
+  bool iconify = FALSE;
+  bool sticky = FALSE;
+  int decoration = KWM::normalDecoration;
+  
+  for (int i = 2; i < argc; i++)
+  {
+      if (!strcmp(argv[i],"-version")) {
+	  printf(KSTART_VERSION);
+	  printf("\n Copyright (C) 1997, 1998 Matthias Ettrich (ettrich@kde.org)\n");
+	  ::exit(0);
+      }
+    if (!strcmp(argv[i],"-window") && i+1 < argc) {
+	window =  argv[++i];
+    }
+    if (!strcmp(argv[i],"-desktop") && i+1 < argc) {
+	QString s =  argv[++i];
+	desktop = s.toInt();
+    }
+    if (!strcmp(argv[i],"-decoration") && i+1 < argc) {
+	QString s =  argv[++i];
+	if (s == "tiny")
+	    decoration = KWM::tinyDecoration;
+	if (s == "none")
+	    decoration = KWM::noDecoration;
+    }
+    if (!strcmp(argv[i],"-activate") ) activate = true;
+    if (!strcmp(argv[i],"-maximize") ) maximize = true;
+    if (!strcmp(argv[i],"-iconify") ) iconify = true;
+    if (!strcmp(argv[i],"-sticky") ) sticky = true;
+  }
+
+  KWMModuleApplication a (argc, argv);
+  
+  fcntl(ConnectionNumber(qt_xdisplay()), F_SETFD, 1);
+
+
+  new KStart(&a, argv[1], window, desktop, activate, maximize, iconify, sticky, decoration);
+
+
+  return a.exec();
+}
---
+++ kstart/kstart.h
@@ -0,0 +1,64 @@
+/*
+ * kstart.h Part of the KDE project.
+ *
+ * Copyright (C) 1997 Matthias Ettrich
+ *
+ */
+
+#include <qapp.h>
+#include <qcursor.h>
+#include <qlist.h>
+#include <qstring.h>
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/time.h>
+#include <X11/X.h>
+#include <X11/Xlib.h>
+#include <qwidget.h>
+#include <qpopmenu.h>
+#include <qstrlist.h>
+#include <kwmmapp.h>
+#include <qdialog.h>
+#include <qlabel.h>
+#include <qpushbt.h>
+
+
+class KStart: public QObject {
+  Q_OBJECT
+
+public:
+  KStart(KWMModuleApplication* kwmmapp_arg,
+	 const char* command_arg,
+	 const char* window_arg,
+	 int desktop_arg,
+	 bool activate_arg,
+	 bool maximize_arg,
+	 bool iconify_arg,
+	 bool sticky_arg,
+	 int decoration_arg);
+  ~KStart(){};
+
+public slots:
+
+  void initialized();
+  void windowAdd(Window);
+
+private:
+    
+  void applyStyle(Window);
+    
+    
+  KWMModuleApplication* kwmmapp;
+  const char* command;
+  const char* window;
+  int desktop;
+  bool activate;
+  bool maximize;
+  bool iconify;
+  bool sticky;
+  int decoration;
+};
+
+
+
