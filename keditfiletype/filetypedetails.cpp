4d977dbd8eab310202616c5a396bcb05d30b1c3d
Nadeem Hasan <nhasan@kde.org>
2003-04-06 00:56:43 +0000

    Dont let it be an orphan.
    svn path=/trunk/kdebase/kcontrol/filetypes/; revision=218578

--- keditfiletype/filetypedetails.cpp
+++ keditfiletype/filetypedetails.cpp
@@ -22,7 +22,7 @@ FileTypeDetails::FileTypeDetails( QWidget * parent, const char * name )
   QVBoxLayout *firstLayout = new QVBoxLayout(firstWidget,KDialog::marginHint(),
                                        KDialog::spacingHint());
 
-  QHBoxLayout *hBox = new QHBoxLayout(0L, 0, KDialog::spacingHint());
+  QHBoxLayout *hBox = new QHBoxLayout(firstWidget, 0, KDialog::spacingHint());
   firstLayout->addLayout(hBox, 1);
 
   iconButton = new KIconButton(firstWidget);
