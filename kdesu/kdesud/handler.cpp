41fbf15fedf4d5dbe8f0d1c70b10833b740a4d21
Waldo Bastian <bastian@kde.org>
2003-09-10 13:28:35 +0000

    Ignore KDE_STARTUP_ENV when checking environment for changes. CCMAIL: 45267-done@bugs.kde.org
    svn path=/trunk/kdebase/kdesu/; revision=249984

--- kdesu/kdesud/handler.cpp
+++ kdesu/kdesud/handler.cpp
@@ -1,5 +1,4 @@
-/* vi: ts=8 sts=4 sw=4
- *
+/*
  * $Id$
  *
  * This file is part of the KDE project, module kdesu.
@@ -38,7 +37,7 @@ extern Repository *repo;
 void kdesud_cleanup();
 
 ConnectionHandler::ConnectionHandler(int fd)
-	: SocketSecurity(fd), m_exitCode(0), m_hasExitCode(false), m_needExitCode(false), m_pid(0)
+        : SocketSecurity(fd), m_exitCode(0), m_hasExitCode(false), m_needExitCode(false), m_pid(0)
 {
     m_Fd = fd;
     m_Priority = 50;
@@ -67,21 +66,21 @@ int ConnectionHandler::handle()
 
     if (nbytes < 0)
     {
-	if (errno == EINTR)
-	    return 0;
-	// read error
-	return -1;
+        if (errno == EINTR)
+            return 0;
+        // read error
+        return -1;
     } else if (nbytes == 0)
     {
-	// eof
-	return -1;
+        // eof
+        return -1;
     }
     tmpbuf[nbytes] = '\000';
 
     if (m_Buf.length()+nbytes > 1024)
     {
-	kdWarning(1205) << "line too long";
-	return -1;
+        kdWarning(1205) << "line too long";
+        return -1;
     }
 
     m_Buf.append(tmpbuf);
@@ -92,19 +91,19 @@ int ConnectionHandler::handle()
     QCString newbuf;
     while ((n = m_Buf.find('\n')) != -1)
     {
-	newbuf = m_Buf.left(n+1);
-	m_Buf.fill('x', n+1);
-	m_Buf.remove(0, n+1);
-	ret = doCommand(newbuf);
-	if (ret < 0)
-	    return ret;
+        newbuf = m_Buf.left(n+1);
+        m_Buf.fill('x', n+1);
+        m_Buf.remove(0, n+1);
+        ret = doCommand(newbuf);
+        if (ret < 0)
+            return ret;
     }
 
     return 0;
 }
 
 QCString ConnectionHandler::makeKey(int _namespace, QCString s1,
-	QCString s2, QCString s3)
+        QCString s2, QCString s3)
 {
     QCString res;
     res.setNum(_namespace);
@@ -131,19 +130,20 @@ void ConnectionHandler::respond(int ok, QCString s)
 
     switch (ok) {
     case Res_OK:
-	buf = "OK";
-	break;
+        buf = "OK";
+        break;
     case Res_NO:
     default:
-	buf = "NO";
-	break;
+        buf = "NO";
+        break;
     }
 
     if (!s.isEmpty())
     {
-	buf += ' ';
-	buf += s;
+        buf += ' ';
+        buf += s;
     }
+
     buf += '\n';
 
     send(m_Fd, buf.data(), buf.length(), 0);
@@ -158,9 +158,9 @@ int ConnectionHandler::doCommand(QCString buf)
 {
     if ((uid_t) peerUid() != getuid())
     {
-	kdWarning(1205) << "Peer uid not equal to me\n";
-	kdWarning(1205) << "Peer: " << peerUid() << " Me: " << getuid() << endl;
-	return -1;
+        kdWarning(1205) << "Peer uid not equal to me\n";
+        kdWarning(1205) << "Peer: " << peerUid() << " Me: " << getuid() << endl;
+        return -1;
     }
 
     QCString key, command, pass, name, user, value, env_check;
@@ -171,159 +171,160 @@ int ConnectionHandler::doCommand(QCString buf)
     switch (tok)
     {
     case Lexer::Tok_pass:  // "PASS password:string timeout:int\n"
-	tok = l->lex();
-	if (tok != Lexer::Tok_str)
-	    goto parse_error;
-	m_Pass.fill('x');
-	m_Pass = l->lval();
-	tok = l->lex();
-	if (tok != Lexer::Tok_num)
-	    goto parse_error;
-	m_Timeout = l->lval().toInt();
-	if (l->lex() != '\n')
-	    goto parse_error;
-	if (m_Pass.isNull())
-	   m_Pass = "";
-	kdDebug(1205) << "Password set!\n";
-	respond(Res_OK);
-	break;
+        tok = l->lex();
+        if (tok != Lexer::Tok_str)
+            goto parse_error;
+        m_Pass.fill('x');
+        m_Pass = l->lval();
+        tok = l->lex();
+        if (tok != Lexer::Tok_num)
+            goto parse_error;
+        m_Timeout = l->lval().toInt();
+        if (l->lex() != '\n')
+            goto parse_error;
+        if (m_Pass.isNull())
+           m_Pass = "";
+        kdDebug(1205) << "Password set!\n";
+        respond(Res_OK);
+        break;
 
     case Lexer::Tok_host:  // "HOST host:string\n"
-	tok = l->lex();
-	if (tok != Lexer::Tok_str)
-	    goto parse_error;
-	m_Host = l->lval();
-	if (l->lex() != '\n')
-	    goto parse_error;
-	kdDebug(1205) << "Host set to " << m_Host << endl;
-	respond(Res_OK);
-	break;
+        tok = l->lex();
+        if (tok != Lexer::Tok_str)
+            goto parse_error;
+        m_Host = l->lval();
+        if (l->lex() != '\n')
+            goto parse_error;
+        kdDebug(1205) << "Host set to " << m_Host << endl;
+        respond(Res_OK);
+        break;
 
     case Lexer::Tok_prio:  // "PRIO priority:int\n"
-	tok = l->lex();
-	if (tok != Lexer::Tok_num)
-	    goto parse_error;
-	m_Priority = l->lval().toInt();
-	if (l->lex() != '\n')
-	    goto parse_error;
-	kdDebug(1205) << "priority set to " << m_Priority << endl;
-	respond(Res_OK);
-	break;
+        tok = l->lex();
+        if (tok != Lexer::Tok_num)
+            goto parse_error;
+        m_Priority = l->lval().toInt();
+        if (l->lex() != '\n')
+            goto parse_error;
+        kdDebug(1205) << "priority set to " << m_Priority << endl;
+        respond(Res_OK);
+        break;
 
     case Lexer::Tok_sched:  // "SCHD scheduler:int\n"
-	tok = l->lex();
-	if (tok != Lexer::Tok_num)
-	    goto parse_error;
-	m_Scheduler = l->lval().toInt();
-	if (l->lex() != '\n')
-	    goto parse_error;
-	kdDebug(1205) << "Scheduler set to " << m_Scheduler << endl;
-	respond(Res_OK);
-	break;
+        tok = l->lex();
+        if (tok != Lexer::Tok_num)
+            goto parse_error;
+        m_Scheduler = l->lval().toInt();
+        if (l->lex() != '\n')
+            goto parse_error;
+        kdDebug(1205) << "Scheduler set to " << m_Scheduler << endl;
+        respond(Res_OK);
+        break;
 
     case Lexer::Tok_exec:  // "EXEC command:string user:string [options:string (env:string)*]\n"
     {
         QCString options;
         QCStringList env;
-	tok = l->lex();
-	if (tok != Lexer::Tok_str)
-	    goto parse_error;
-	command = l->lval();
-	tok = l->lex();
-	if (tok != Lexer::Tok_str)
-	    goto parse_error;
-	user = l->lval();
-	tok = l->lex();
-	if (tok != '\n')
-	{
-	    if (tok != Lexer::Tok_str)
-		goto parse_error;
-	    options = l->lval();
+        tok = l->lex();
+        if (tok != Lexer::Tok_str)
+            goto parse_error;
+        command = l->lval();
+        tok = l->lex();
+        if (tok != Lexer::Tok_str)
+            goto parse_error;
+        user = l->lval();
+        tok = l->lex();
+        if (tok != '\n')
+        {
+            if (tok != Lexer::Tok_str)
+                goto parse_error;
+            options = l->lval();
             tok = l->lex();
-	    while (tok != '\n')
+            while (tok != '\n')
             {
                if (tok != Lexer::Tok_str)
-	           goto parse_error;
-	       QCString env_str = l->lval();
-	       env.append(env_str);
-	       env_check += "*"+env_str;
+                   goto parse_error;
+               QCString env_str = l->lval();
+               env.append(env_str);
+               if (strncmp(env_str, "KDE_STARTUP_ENV=", strlen("KDE_STARTUP_ENV=")) != 0)
+                   env_check += "*"+env_str;
                tok = l->lex();
             }
-	}
+        }
 
-	QCString auth_user;
-	if ((m_Scheduler != SuProcess::SchedNormal) || (m_Priority > 50))
-	    auth_user = "root";
-	else
-	    auth_user = user;
-	key = makeKey(2, m_Host, auth_user, command);
-	// We only use the command if the environment is the same.
-	if (repo->find(key) == env_check)
-	{
+        QCString auth_user;
+        if ((m_Scheduler != SuProcess::SchedNormal) || (m_Priority > 50))
+            auth_user = "root";
+        else
+            auth_user = user;
+        key = makeKey(2, m_Host, auth_user, command);
+        // We only use the command if the environment is the same.
+        if (repo->find(key) == env_check)
+        {
            key = makeKey(0, m_Host, auth_user, command);
            pass = repo->find(key);
         }
-	if (pass.isNull()) // isNull() means no password, isEmpty() can mean empty password
-	{
-	    if (m_Pass.isNull())
-	    {
-		respond(Res_NO);
-		break;
-	    }
-	    data.value = env_check;
-	    data.timeout = m_Timeout;
+        if (pass.isNull()) // isNull() means no password, isEmpty() can mean empty password
+        {
+            if (m_Pass.isNull())
+            {
+                respond(Res_NO);
+                break;
+            }
+            data.value = env_check;
+            data.timeout = m_Timeout;
             key = makeKey(2, m_Host, auth_user, command);
-	    repo->add(key, data);
-	    data.value = m_Pass;
-	    data.timeout = m_Timeout;
+            repo->add(key, data);
+            data.value = m_Pass;
+            data.timeout = m_Timeout;
             key = makeKey(0, m_Host, auth_user, command);
-	    repo->add(key, data);
-	    pass = m_Pass;
-	}
+            repo->add(key, data);
+            pass = m_Pass;
+        }
 
-	// Execute the command asynchronously
-	kdDebug(1205) << "Executing command: " << command << endl;
-	pid_t pid = fork();
-	if (pid < 0)
-	{
-	    kdDebug(1205) << "fork(): " << strerror(errno) << endl;
-	    respond(Res_NO);
-	    break;
-	} else if (pid > 0)
-	{
-	    m_pid = pid;
-	    respond(Res_OK);
-	    break;
-	}
+        // Execute the command asynchronously
+        kdDebug(1205) << "Executing command: " << command << endl;
+        pid_t pid = fork();
+        if (pid < 0)
+        {
+            kdDebug(1205) << "fork(): " << strerror(errno) << endl;
+            respond(Res_NO);
+            break;
+        } else if (pid > 0)
+        {
+            m_pid = pid;
+            respond(Res_OK);
+            break;
+        }
 
-	// Ignore SIGCHLD because "class SuProcess" needs waitpid()
-	signal(SIGCHLD, SIG_DFL);
+        // Ignore SIGCHLD because "class SuProcess" needs waitpid()
+        signal(SIGCHLD, SIG_DFL);
 
-	int ret;
-	if (m_Host.isEmpty())
-	{
-	    SuProcess proc;
-	    proc.setCommand(command);
-	    proc.setUser(user);
-	    if (options.contains('x'))
-	       proc.setXOnly(true);
-	    if (options.contains('f'))
-	       proc.setDCOPForwarding(true);
-	    proc.setPriority(m_Priority);
-	    proc.setScheduler(m_Scheduler);
-	    proc.setEnvironment(env);
-	    ret = proc.exec(pass.data());
-	} else
-	{
-	    SshProcess proc;
-	    proc.setCommand(command);
-	    proc.setUser(user);
-	    proc.setHost(m_Host);
-	    ret = proc.exec(pass.data());
-	}
+        int ret;
+        if (m_Host.isEmpty())
+        {
+            SuProcess proc;
+            proc.setCommand(command);
+            proc.setUser(user);
+            if (options.contains('x'))
+               proc.setXOnly(true);
+            if (options.contains('f'))
+               proc.setDCOPForwarding(true);
+            proc.setPriority(m_Priority);
+            proc.setScheduler(m_Scheduler);
+            proc.setEnvironment(env);
+            ret = proc.exec(pass.data());
+        } else
+        {
+            SshProcess proc;
+            proc.setCommand(command);
+            proc.setUser(user);
+            proc.setHost(m_Host);
+            ret = proc.exec(pass.data());
+        }
 
-	kdDebug(1205) << "Command completed: " << command << endl;
-	_exit(ret);
+        kdDebug(1205) << "Command completed: " << command << endl;
+        _exit(ret);
     }
 
     case Lexer::Tok_delCmd:  // "DEL command:string user:string\n"
@@ -441,63 +442,63 @@ int ConnectionHandler::doCommand(QCString buf)
     break;
 
     case Lexer::Tok_getKeys:  // "GETK groupname:string\n"
-	tok = l->lex();
-	if (tok != Lexer::Tok_str)
-	    goto parse_error;
-	name = l->lval();
-	if (l->lex() != '\n')
-	    goto parse_error;
-	kdDebug(1205) << "Request for group key: " << name << endl;
-	value = repo->findKeys(name);
-	if (!value.isEmpty())
-	    respond(Res_OK, value);
-	else
-	    respond(Res_NO);
-	break;
+        tok = l->lex();
+        if (tok != Lexer::Tok_str)
+            goto parse_error;
+        name = l->lval();
+        if (l->lex() != '\n')
+            goto parse_error;
+        kdDebug(1205) << "Request for group key: " << name << endl;
+        value = repo->findKeys(name);
+        if (!value.isEmpty())
+            respond(Res_OK, value);
+        else
+            respond(Res_NO);
+        break;
 
     case Lexer::Tok_chkGroup:  // "CHKG groupname:string\n"
-	tok = l->lex();
-	if (tok != Lexer::Tok_str)
-	    goto parse_error;
-	name = l->lval();
-	if (l->lex() != '\n')
-	    goto parse_error;
-	kdDebug(1205) << "Checking for group key: " << name << endl;
-	if ( repo->hasGroup( name ) < 0 )
-	    respond(Res_NO);
-	else
-	    respond(Res_OK);
-	break;
+        tok = l->lex();
+        if (tok != Lexer::Tok_str)
+            goto parse_error;
+        name = l->lval();
+        if (l->lex() != '\n')
+            goto parse_error;
+        kdDebug(1205) << "Checking for group key: " << name << endl;
+        if ( repo->hasGroup( name ) < 0 )
+            respond(Res_NO);
+        else
+            respond(Res_OK);
+        break;
 
     case Lexer::Tok_ping:  // "PING\n"
-	tok = l->lex();
-	if (tok != '\n')
-	    goto parse_error;
-	respond(Res_OK);
-	break;
+        tok = l->lex();
+        if (tok != '\n')
+            goto parse_error;
+        respond(Res_OK);
+        break;
 
     case Lexer::Tok_exit:  // "EXIT\n"
-	tok = l->lex();
-	if (tok != '\n')
-	    goto parse_error;
-	m_needExitCode = true;
+        tok = l->lex();
+        if (tok != '\n')
+            goto parse_error;
+        m_needExitCode = true;
         if (m_hasExitCode)
            sendExitCode();
-	break;
+        break;
 
     case Lexer::Tok_stop:  // "STOP\n"
-	tok = l->lex();
-	if (tok != '\n')
-	    goto parse_error;
-	kdDebug(1205) << "Stopping by command" << endl;
-	respond(Res_OK);
-	kdesud_cleanup();
-	exit(0);
+        tok = l->lex();
+        if (tok != '\n')
+            goto parse_error;
+        kdDebug(1205) << "Stopping by command" << endl;
+        respond(Res_OK);
+        kdesud_cleanup();
+        exit(0);
 
     default:
-	kdWarning(1205) << "Unknown command: " << l->lval() << endl;
-	respond(Res_NO);
-	goto parse_error;
+        kdWarning(1205) << "Unknown command: " << l->lval() << endl;
+        respond(Res_NO);
+        goto parse_error;
     }
 
     delete l;
