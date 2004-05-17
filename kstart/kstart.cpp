bf0c1ba21cbefd8e117ed24dfcc93d4fbb089e83
Luboš Luňák <l.lunak@kde.org>
2004-05-17 14:52:44 +0000

    #81585.
    svn path=/trunk/kdebase/kstart/; revision=312072

--- kstart/kstart.cpp
+++ kstart/kstart.cpp
@@ -193,8 +193,9 @@ static KCmdLineOptions options[] =
   { "!+command", I18N_NOOP("Command to execute."), 0 },
   // "!" means: all options after command are treated as arguments to the command
   { "window <regexp>", I18N_NOOP("A regular expression matching the window title.\n"
-                  "If you do not specify one, then the very first window\n"
-                  "to appear will be taken. NOT RECOMMENDED!"), 0 },
+                  "If you do not specify one, then the very first\n"
+                  "window to appear will be taken.\n"
+                  "Omitting this option is NOT RECOMMENDED!"), 0 },
   { "desktop <number>", I18N_NOOP("Desktop where to make the window appear. "), 0 },
   { "currentdesktop", I18N_NOOP("Make the window appear on the desktop that was active\nwhen starting the application. "), 0 },
   { "alldesktops", I18N_NOOP("Make the window appear on all desktops"), 0 },
