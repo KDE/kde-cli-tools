4561c4a6f187581bc8dd55fafccbf6ae64478423
İsmail Dönmez <ismail@kde.org>
2005-07-23 08:45:03 +0000

    Compile, really
    svn path=/trunk/KDE/kdebase/kdesu/; revision=437838

--- kdesu/kdesud/secure.cpp
+++ kdesu/kdesud/secure.cpp
@@ -19,6 +19,7 @@
 #include <sys/socket.h>
 
 #include <kdebug.h>
+#include <ksockaddr.h>
 #include "secure.h"
 
 
@@ -30,7 +31,7 @@
 
 SocketSecurity::SocketSecurity(int sockfd)
 {
-    size_t len = sizeof(struct ucred);
+    ksocklen_t len = sizeof(struct ucred);
     if (getsockopt(sockfd, SOL_SOCKET, SO_PEERCRED, &cred, &len) < 0) {
 	kdError() << "getsockopt(SO_PEERCRED) " << perror << endl;
 	return;
