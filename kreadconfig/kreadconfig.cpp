ae44cc259fbc01e25565f6763912b08d50a7daf0
John Layt <john@layt.net>
2009-12-28 21:00:38 +0000

    krazy fixes
    svn path=/trunk/KDE/kdebase/runtime/; revision=1067076

--- kreadconfig/kreadconfig.cpp
+++ kreadconfig/kreadconfig.cpp
@@ -1,7 +1,24 @@
-/* Read KConfig() entries - for use in shell scripts.
- * (c) 2001 Red Hat, Inc.
- * Programmed by Bernhard Rosenkraenzer <bero@redhat.com>
- *
+/*  Read KConfig() entries - for use in shell scripts.
+
+    Copyright (c) 2001 Red Hat, Inc.
+
+    Programmed by Bernhard Rosenkraenzer <bero@redhat.com>
+
+    This program is free software; you can redistribute it and/or
+    modify it under the terms of the GNU General Public License as
+    published by the Free Software Foundation; either version 2 of
+    the License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/*
  * If --type is specified as bool, the return value is 0 if the value
  * is set, 1 if it isn't set. There is no output.
  *
