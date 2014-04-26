4b54c5310767146d902451473b3d8d68c9cbd32f
Aleix Pol <aleixpol@kde.org>
2014-04-26 14:14:44 +0200

    kreadconfig and kwriteconfig were moved to kconfig

--- kreadconfig/Messages.sh
+++
@@ -1,4 +0,0 @@
-#! /usr/bin/env bash
-### TODO: why do we need 2 POT files for one directory?
-$XGETTEXT kreadconfig.cpp -o $podir/kreadconfig.pot
-$XGETTEXT kwriteconfig.cpp -o $podir/kwriteconfig.pot
