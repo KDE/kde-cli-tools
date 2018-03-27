4d48ba71bf513df0773a487a4f8b59f68116e44a
Fabian Vogt <fabian@ritter-vogt.de>
2018-03-27 21:31:39 +0200

    Fix build in kcmshell5, accidentially landed the wrong revision
    Needs to be _argc, _argv.

--- kcmshell/main.cpp
+++ kcmshell/main.cpp
@@ -177,7 +177,7 @@ void KCMShell::appExit(const QString &appId, const QString &oldName, const QStri
 extern "C" Q_DECL_EXPORT int kdemain(int _argc, char *_argv[])
 {
     const bool qpaVariable = qEnvironmentVariableIsSet("QT_QPA_PLATFORM");
-    KWorkSpace::detectPlatform(argc, argv);
+    KWorkSpace::detectPlatform(_argc, _argv);
     KCMShell app(_argc, _argv);
     if (!qpaVariable) {
         // don't leak the env variable to processes we start
