74ea47b814ac42e2354c308d4a38490d53bdfd9a
Nicolas Fella <nicolas.fella@gmx.de>
2023-03-03 17:23:05 +0100

    Drop deprecated code blocks

--- keditfiletype/kserviceselectdlg.cpp
+++ keditfiletype/kserviceselectdlg.cpp
@@ -13,24 +13,10 @@
 
 #include <KLocalizedString>
 
-#if KSERVICE_BUILD_DEPRECATED_SINCE(5, 102)
-KService::List allParts()
-{
-    KService::List result;
-    const KService::List allServices = KService::allServices();
-    for (const auto &service : allServices) {
-        if (service->hasServiceType(QStringLiteral("KParts/ReadOnlyPart"))) {
-            result << service;
-        }
-    }
-    return result;
-}
-#else
 QVector<KPluginMetaData> allParts()
 {
     return KPluginMetaData::findPlugins(QStringLiteral("kf" QT_STRINGIFY(QT_VERSION_MAJOR) "/parts"));
 }
-#endif
 
 KServiceSelectDlg::KServiceSelectDlg(const QString & /*serviceType*/, const QString & /*value*/, QWidget *parent)
     : QDialog(parent)
@@ -49,11 +35,7 @@ KServiceSelectDlg::KServiceSelectDlg(const QString & /*serviceType*/, const QStr
     const auto parts = allParts();
 
     for (const auto &part : parts) {
-#if KSERVICE_BUILD_DEPRECATED_SINCE(5, 102)
-        m_listbox->addItem(new KServiceListItem(part, KServiceListWidget::SERVICELIST_SERVICES));
-#else
         m_listbox->addItem(new PluginListItem(part));
-#endif
     }
 
     m_listbox->model()->sort(0);
